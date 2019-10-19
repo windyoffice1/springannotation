b9fbe230adfc2d752214d5782fb2cf5853643d5e
windyoffice <test@126.com>
2019-10-19 21:43:29 +0800

    @Scope作用域

--- src/main/java/com/windyoffice/config/MainConfig.java
+++ src/main/java/com/windyoffice/config/MainConfig.java
@@ -3,18 +3,22 @@ package com.windyoffice.config;
 
 import com.windyoffice.bean.Person;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import sun.awt.SunHints;
 
 @Configuration
 @ComponentScan("com.windyoffice")
 public class MainConfig {
 
-    //给容器中注册一个Bean 类型为方法返回值类型 ，ID为方法名
-    @Bean(name = "person02")
+    //给容器中注册一个Bean 类型为方法返回值类型 ，
+    @Scope(value = "aaa")
+    @Bean
     public Person person(){
+        System.out.println("给容器中添加person====================");
         return new Person("李四",20);
     }
 }
--- src/test/java/com/windyoffice/bean/BeanConfigTest.java
+++ src/test/java/com/windyoffice/bean/BeanConfigTest.java
@@ -10,11 +10,16 @@ public class BeanConfigTest {
     @Test
     public void testGetBean(){
         AnnotationConfigApplicationContext applicationContext=new AnnotationConfigApplicationContext(MainConfig.class);
-        Person person=applicationContext.getBean(Person.class);
-        System.out.println(person);
+/*        Person person=applicationContext.getBean(Person.class);
+        System.out.println(person);*/
         String [] beanNames=applicationContext.getBeanNamesForType(Person.class);
         for (String beanName :beanNames){
             System.out.println(beanName);
         }
+        Person person=applicationContext.getBean(Person.class);
+        //spring Bean 默认是单实例
+     /*   Person person1=applicationContext.getBean(Person.class);
+        Person person2=applicationContext.getBean(Person.class);
+        System.out.println(person1==person2);*/
     }
 }
